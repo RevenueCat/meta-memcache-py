c844e8c42407ac77c5f7396d1a24349541c89b02
Guillermo Perez <bisho@revenuecat.com>
2023-09-12 11:33:56 +0200

    Add ephemeral pool
    ## Motivation / Description
    A pool that will adjust the ttl of all commands to be less or equal
    than the given max ttl.

---
+++ tests/ephemeral_cache_client_test.py
@@ -0,0 +1,38 @@
+from unittest.mock import call
+from meta_memcache.protocol import NOOP, Flag, IntFlag
+
+from pytest_mock import MockerFixture
+
+from meta_memcache import (
+    Key,
+    ServerAddress,
+    CacheClient,
+    connection_pool_factory_builder,
+)
+
+
+def test_ephemeral_cache_client(mocker: MockerFixture) -> None:
+    socket = mocker.patch("meta_memcache.configuration.socket", autospec=True)
+    c = socket.socket()
+    cache_client = CacheClient.ephemeral_cache_client_from_servers(
+        servers=[
+            ServerAddress(host="ok1", port=11211),
+            ServerAddress(host="ok2", port=11211),
+        ],
+        max_ttl=60,
+        connection_pool_factory_fn=connection_pool_factory_builder(initial_pool_size=2),
+    )
+
+    cache_client.set(key=Key("foo"), value=1, ttl=1000, no_reply=True)
+    c.sendall.assert_called_once_with(b"ms foo 1 q T60 F2\r\n1\r\n" + NOOP)
+    c.sendall.reset_mock()
+
+    cache_client.meta_multiget(
+        keys=[Key("bar"), Key("foo")],
+        flags={
+            Flag.NOREPLY,
+        },
+        int_flags={IntFlag.CACHE_TTL: 1000},
+    )
+    c.sendall.assert_has_calls([call(b"mg bar q T60\r\n"), call(b"mg foo q T60\r\n")])
+    c.sendall.reset_mock()
